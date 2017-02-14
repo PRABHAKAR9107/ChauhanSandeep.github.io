3ceb2ca8f35bdaab8c0aac47a215160a7080eb27
ChauhanSandeep <schauhan346@gmail.com>
2017-02-14 12:08:40 -0500

    added Description and form tab in rafting page. Made header and footer consistent with index page

--- basic.js
+++ basic.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
 	$(".button-collapse").sideNav();
  	$('.carousel.carousel-slider').carousel({full_width: true});
 	$('.parallax').parallax();
+
+	 $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
     });
