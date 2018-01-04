1d493de12a5c7ae2e6febe367ce200f0e1a6ad36
Sandeep Chauhan <schauhan346@gmail.com>
2018-01-03 19:36:04 -0500

    Delete basic.js

--- basic.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-      $(".dropdown-button").dropdown();
-	$(".button-collapse").sideNav();
- 	$('.carousel.carousel-slider').carousel({full_width: true});
-	$('.parallax').parallax();
-    });
